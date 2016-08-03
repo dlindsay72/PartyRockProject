f272f9b97bbe2cc57d0493521f4c0ef30b639627
Dan Lindsay <dlindsay72@hotmail.com>
2016-08-03 11:15:37 -0400

    created folders for Model, View and Controller and changed name of first viewconroller

---
+++ MansionOfParty/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MansionOfParty
+//
+//  Created by Dan Lindsay on 2016-08-03.
+//  Copyright © 2016 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+
+}
+
